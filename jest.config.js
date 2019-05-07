a96e68aa7871278aa4ed67e95bb3fb793b685bfb
Steev Sachs <steev.sachs@gmail.com>
2019-05-07 10:59:10 -0400

    Initial commit

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  coveragePathIgnorePatterns: [
+    '<rootDir>/node_modules/',
+    '<rootDir>/test-utils/',
+  ],
+  setupFilesAfterEnv: ['<rootDir>/scripts/setupTests.js'],
+  testEnvironment: 'jsdom-global',
+  testPathIgnorePatterns: ['/utils/'],
+  testRegex: 'src/.*(/__tests__/[^.]+.(?!integration)|\\.(test|spec))\\.tsx?$',
+  transform: {
+    '\\.tsx?$': 'babel-jest',
+  },
+}
---
+++ src/index.tsx
@@ -0,0 +1,5 @@
+import * as ReactDOM from 'react-dom'
+import React from 'react'
+import Root from './components/Root'
+
+ReactDOM.render(<Root />, document.getElementById('root'))
